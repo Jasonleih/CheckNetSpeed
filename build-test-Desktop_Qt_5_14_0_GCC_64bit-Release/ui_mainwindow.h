bd9e94ed3a6a8fc7b51fab72a0843376f6e73650
jasonleih <707809370@qq.com>
2020-01-13 19:02:38 +0800

    Linux下的网速检测小程序

---
+++ build-test-Desktop_Qt_5_14_0_GCC_64bit-Release/ui_mainwindow.h
@@ -0,0 +1,89 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextBrowser>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *action;
+    QWidget *centralwidget;
+    QLabel *label;
+    QLabel *label_2;
+    QTextBrowser *textBrowser;
+    QTextBrowser *updata;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(350, 50);
+        QSizePolicy sizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(MainWindow->sizePolicy().hasHeightForWidth());
+        MainWindow->setSizePolicy(sizePolicy);
+        MainWindow->setMinimumSize(QSize(350, 50));
+        MainWindow->setMaximumSize(QSize(350, 50));
+        MainWindow->setLayoutDirection(Qt::LeftToRight);
+        action = new QAction(MainWindow);
+        action->setObjectName(QString::fromUtf8("action"));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(160, 0, 51, 21));
+        label_2 = new QLabel(centralwidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setGeometry(QRect(0, 0, 54, 17));
+        textBrowser = new QTextBrowser(centralwidget);
+        textBrowser->setObjectName(QString::fromUtf8("textBrowser"));
+        textBrowser->setGeometry(QRect(50, 0, 101, 31));
+        updata = new QTextBrowser(centralwidget);
+        updata->setObjectName(QString::fromUtf8("updata"));
+        updata->setGeometry(QRect(210, 0, 121, 31));
+        MainWindow->setCentralWidget(centralwidget);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "\345\256\236\346\227\266\347\275\221\351\200\237", nullptr));
+        action->setText(QCoreApplication::translate("MainWindow", "\346\237\245\346\211\276", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "\344\270\212\344\274\240\351\200\237\345\272\246", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "\344\270\213\350\275\275\351\200\237\345\272\246", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
